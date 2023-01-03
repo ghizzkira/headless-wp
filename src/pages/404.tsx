e93017f6af41d4ca6ea6c6749d876479e3e18de9
Karyana Yandi <karyana@yopun.com>
2023-01-03 15:03:35 +0700

    feat: add 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,8 @@
 import env from "@/env"
 import Head from "next/head"
-// import NextLink from "next/link"
+import NextLink from "next/link"
 import { useRouter } from "next/router"
+import { Button } from "@/ui"
 
 export default function Custom404() {
   const router = useRouter()
@@ -17,9 +18,25 @@ export default function Custom404() {
           href={`https://${env.DOMAIN}${router.pathname}`}
         />
       </Head>
-      <div className="flex items-center justify-center h-screen bg-white">
-        404
-      </div>
+      <section className="bg-white dark:bg-gray-900">
+        <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+          <div className="mx-auto max-w-screen-sm text-center">
+            <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-blue-600 dark:text-blue-500">
+              404
+            </h1>
+            <p className="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">
+              Something's missing.
+            </p>
+            <p className="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">
+              Sorry, we can't find that page. You'll find lots to explore on the
+              home page.
+            </p>
+            <NextLink href="/">
+              <Button colorScheme="blue">Back to Homepage</Button>
+            </NextLink>
+          </div>
+        </div>
+      </section>
     </>
   )
 }
