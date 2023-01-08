80ccb09de44d0a0447e55b234917e89faab6a5f8
Karyana Yandi <karyana@yopun.com>
2023-01-08 10:59:52 +0700

    fix: home type

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,7 +9,17 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <style jsx global>{`
-        html {
+        body {
+          font-family: -apple-system, BlinkMacSystemFont, segoe ui,
+            helvetica neue, Arial, noto sans, sans-serif, apple color emoji,
+            segoe ui emoji, segoe ui symbol, noto color emoji !important;
+        }
+        h1,
+        h2,
+        h3,
+        h4,
+        h5,
+        h6 {
           font-family: ${inter.style.fontFamily};
         }
         .scrollbar::-webkit-scrollbar {
@@ -31,6 +41,7 @@ function App({ Component, pageProps }: AppProps) {
           background: #f1f1f1;
         }
       `}</style>
+
       <ThemeProvider attribute="class" storageKey="theme" enableSystem>
         <Component {...pageProps} />
       </ThemeProvider>
