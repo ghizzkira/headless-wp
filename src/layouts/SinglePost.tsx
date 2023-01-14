28b1835e872899170954ee11e03064f9b465fc59
Karyana Yandi <karyana@yopun.com>
2023-01-14 21:05:28 +0700

    feat: styling single post

---
+++ src/layouts/SinglePost.tsx
@@ -0,0 +1,50 @@
+import * as React from "react"
+import { Container, useDisclosure } from "@/ui"
+import { TopNav, SideNav } from "@/components/Navigation"
+import { Footer } from "@/components/Footer"
+import { wpGetPrimaryMenus } from "@/lib/wp-menus"
+interface HeaderProps {
+  children: React.ReactNode
+}
+
+export const SinglePostLayout = React.forwardRef<HTMLDivElement, HeaderProps>(
+  (props) => {
+    const { isOpen, onToggle } = useDisclosure()
+    const [primaryMenus, setPrimaryMenus] = React.useState<any>(null)
+    React.useEffect(() => {
+      async function menus() {
+        const { menu } = await wpGetPrimaryMenus()
+        setPrimaryMenus(menu)
+      }
+      menus()
+    }, [])
+
+    const { children } = props
+
+    return (
+      <>
+        <TopNav onToggle={onToggle} />
+        <div
+          className={`${
+            isOpen ? "-translate-x-full" : "translate-x-0"
+          } transition ease-in-out delay-150 border-r border-gray-100 bg-white z-20 fixed pt-20 top-0 overflow-x-auto h-full flex flex-row bg-gray-100 w-[250px] scrollbar`}
+        >
+          <SideNav primaryMenus={primaryMenus} />
+        </div>
+        <div
+          id="container"
+          className={`flex mt-20 ${
+            isOpen ? "pl-0" : "pl-[250px]"
+          } transition-[padding] ease-in-out delay-150`}
+        >
+          <Container>{children}</Container>
+        </div>
+        <Footer
+          className={`transition-[padding] ease-in-out delay-150 ${
+            isOpen ? "pl-0" : "pl-[250px]"
+          }`}
+        />
+      </>
+    )
+  },
+)
--- src/pages/[category]/[slug]/index.tsx
+++ src/pages/[category]/[slug]/index.tsx
@@ -1,9 +1,9 @@
 import { wpGetPostBySlug, wpGetAllPosts } from "@/lib/wp-posts"
 import { wpPrimaryCategorySlug } from "@/lib/wp-categories"
-import { Layout } from "@/components/Layout"
+import { SinglePostLayout } from "@/layouts/SinglePost"
 import { PostCardSide } from "@/components/Card/PostCardSide"
 import NextImage from "next/image"
-import { Heading, Button } from "@/ui"
+import { Heading, Button, ButtonGroup } from "@/ui"
 import NextLink from "next/link"
 import { MetadataPost } from "@/components/Metadata/MetaDataPost"
 import { GetServerSideProps } from "next"
@@ -35,62 +35,69 @@ export default function Post(props: PostProps) {
   const { content, title, author, categories, featuredImage, date } = post
 
   return (
-    <Layout>
-      <section className="mx-8 flex flex-row">
-        <div className="pr-4">
-          <div>
-            {categories.map((category: { slug: string; name: string }) => {
-              return (
-                <Button variant="ghost" className="rounded-full">
-                  <NextLink href={`/${category.slug}`}>
-                    {category.name}
-                  </NextLink>
-                </Button>
-              )
-            })}
-          </div>
-          <Heading
-            as="h1"
-            className="mt-4 mb-2 border-b border-gray-200 pb-2 text-2xl font-bold leading-[36px] dark:border-gray-600 md:border-none md:text-3xl md:leading-[43px]"
-            dangerouslySetInnerHTML={{
-              __html: title,
-            }}
-          />
-          <div>
-            <MetadataPost
-              authorName={author.name}
-              authorAvatarUrl={author.avatar.url}
-              authorSlug={author.slug}
-              date={date}
+    <SinglePostLayout>
+      <div className="flex">
+        <section className="mx-8 flex flex-row w-8/12">
+          <div className="pr-4">
+            <div>
+              {categories.map((category: { slug: string; name: string }) => {
+                return (
+                  <ButtonGroup className="px-1">
+                    <Button
+                      size="xs"
+                      colorScheme="slate"
+                      className="!rounded-full uppercase"
+                    >
+                      <NextLink href={`/${category.slug}`}>
+                        {category.name}
+                      </NextLink>
+                    </Button>
+                  </ButtonGroup>
+                )
+              })}
+            </div>
+            <Heading
+              as="h1"
+              className="mt-4 mb-2 border-b border-gray-200 pb-2 text-2xl font-bold leading-[36px] dark:border-gray-600 md:border-none md:text-3xl md:leading-[43px]"
+              dangerouslySetInnerHTML={{
+                __html: title,
+              }}
+            />
+            <div>
+              <MetadataPost
+                authorName={author.name}
+                authorAvatarUrl={author.avatar.url}
+                authorSlug={author.slug}
+                date={date}
+              />
+            </div>
+            {featuredImage && (
+              <>
+                <NextImage
+                  width="1280"
+                  height="720"
+                  alt={featuredImage.altText}
+                  className="rounded-lg object-cover"
+                  src={featuredImage.sourceUrl}
+                />
+                {featuredImage.caption && (
+                  <span
+                    className="text-center text-xs italic text-gray-600 dark:text-gray-500"
+                    dangerouslySetInnerHTML={{
+                      __html: featuredImage.caption,
+                    }}
+                  />
+                )}
+              </>
+            )}
+            <section
+              className="article-body"
+              dangerouslySetInnerHTML={{
+                __html: content,
+              }}
             />
           </div>
-          {featuredImage && (
-            <>
-              <NextImage
-                width="1280"
-                height="720"
-                alt={featuredImage.altText}
-                className="featured-thumbnail object-cover"
-                src={featuredImage.sourceUrl}
-              />
-              {featuredImage.caption && (
-                <span
-                  className="text-center text-xs italic text-gray-600 dark:text-gray-500"
-                  dangerouslySetInnerHTML={{
-                    __html: featuredImage.caption,
-                  }}
-                />
-              )}
-            </>
-          )}
-          <section
-            className="article-body"
-            dangerouslySetInnerHTML={{
-              __html: content,
-            }}
-          />
-        </div>
-
+        </section>
         <aside className="w-4/12">
           <div className="rounded-xl border border-gray-100 p-4 sticky top-8">
             <div className="mb-4">
@@ -126,8 +133,8 @@ export default function Post(props: PostProps) {
             )}
           </div>
         </aside>
-      </section>
-    </Layout>
+      </div>
+    </SinglePostLayout>
   )
 }
 
